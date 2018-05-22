af53468bad4f8f34214338dd324eabe0d4f740f0
Unknown <romain.romss@gmail.com>
2018-05-22 22:48:47 +0200

    fix(Tests): update handlers

--- tests/UI/Form/Handler/AddTrickTypeHandlerTest.php
+++ tests/UI/Form/Handler/AddTrickTypeHandlerTest.php
@@ -15,10 +15,12 @@ namespace App\Tests\UI\Form\Handler;
 
 
 use App\Domain\Builder\TrickBuilder;
+use App\Domain\DTO\AddTrickDTO;
 use App\Domain\Repository\TricksRepository;
 use App\UI\Form\Handler\AddTrickTypeHandler;
 use App\UI\Form\Handler\Intefaces\AddTrickTypeHandlerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 /**
@@ -43,12 +45,18 @@ class AddTrickTypeHandlerTest extends KernelTestCase
 	 */
 	private $tokenstorage;
 
+	/**
+	 * @var FormInterface
+	 */
+	private $formInterface;
+
 	public function setUp()
 	{
 		static::BootKernel();
 		$this->tricksBuilder = $this->createMock(TrickBuilder::class);
 		$this->tricksRepository = $this->createMock(TricksRepository::class);
-		$this->tokenstorage = static::$kernel->getContainer()->get('security.token_storage') ;
+		$this->tokenstorage = static::$kernel->getContainer()->get('security.token_storage');
+		$this->formInterface = $this->createMock(FormInterface::class);
 	}
 
 
@@ -56,5 +64,66 @@ class AddTrickTypeHandlerTest extends KernelTestCase
 	{
 		$addTricksTypeHandler = new AddTrickTypeHandler($this->tricksBuilder, $this->tricksRepository, $this->tokenstorage);
 		static::assertInstanceOf(AddTrickTypeHandlerInterface::class, $addTricksTypeHandler);
+	}
+
+	/**
+	 * @throws \Doctrine\ORM\ORMException
+	 * @throws \Doctrine\ORM\OptimisticLockException
+	 */
+	public function testHandleReturnFalse()
+	{
+		$addTricksTypeHandler = new AddTrickTypeHandler(
+			$this->tricksBuilder,
+			$this->tricksRepository,
+			$this->tokenstorage
+		);
+
+		static::assertInstanceOf(
+			AddTrickTypeHandlerInterface::class,
+			$addTricksTypeHandler
+		);
+
+		static::assertFalse(
+			$addTricksTypeHandler->handle($this->formInterface)
+		);
+	}
+
+
+	/**
+	 * @throws \Doctrine\ORM\ORMException
+	 * @throws \Doctrine\ORM\OptimisticLockException
+	 */
+	public function testHandleReturnTrue()
+	{
+		$addTrickDTO = new AddTrickDTO(
+			'name',
+			'description',
+			'group',
+			'slug',
+			['pictures'],
+			['movies']
+		);
+
+		$this->formInterface->method('isValid')->willReturn(true);
+		$this->formInterface->method('isSubmitted')->willReturn(true);
+		$this->formInterface->method('getData')->willReturn($addTrickDTO);
+
+
+		$addTrickTypeHandler = new AddTrickTypeHandler(
+			$this->tricksBuilder,
+			$this->tricksRepository,
+			$this->tokenstorage
+		);
+
+		$this->tricksRepository->save($this->tricksBuilder->getTrick());
+
+		static::assertInstanceOf(
+			AddTrickTypeHandlerInterface::class,
+			$addTrickTypeHandler
+		);
+
+		static::assertTrue(
+			$addTrickTypeHandler->handle($this->formInterface)
+		);
 	}
 }
