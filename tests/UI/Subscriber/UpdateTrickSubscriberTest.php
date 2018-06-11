9f3b7b64cf4d363029da48877fca8b4fdbb5a4bd
romain <romain.romss@gmail.com>
2018-06-11 15:24:23 +0200

    test(UpdateTrickSubscriber: Add new test)

---
+++ tests/UI/Subscriber/UpdateTrickSubscriberTest.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+/*
+ * This file is part of the Snowtricks project.
+ *
+ * (c) Romain Bayette <romain.romss@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Tests\UI\Subscriber;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class UpdateTrickSubscriberTest.
+ *
+ * @author Romain Bayette <romain.romss@gmail.com>
+ */
+class UpdateTrickSubscriberTest extends TestCase
+{
+	
+}
