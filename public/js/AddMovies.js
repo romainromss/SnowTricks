97692cfa71b6e4d6fa0953beab71bb6f2dde26ee
romain <romain.romss@gmail.com>
2018-06-15 16:10:38 +0200

    js(AddMovies + AddPictures): Fix errors for update and add

--- public/js/AddMovies.js
+++ public/js/AddMovies.js
@@ -1,25 +1,69 @@
-parameters = {
-    form:document.getElementById('add_tricks'),
-    container:document.getElementById('add_tricks_movies'),
+parametersMovies = {
+    container:document.querySelector('div#add_trick_movies'),
     property: document.querySelectorAll("[data-prototype]"),
 }
 
-let movie = 0;
-
-function addMovie() {
-    const template = parameters.container.dataset.prototype
-    const el = template.replace(/__name__label__/g, 'video n°' + (movie+1))
-        .replace(/__name__/g, index)
-    parameters.form.appendChild(createElementFromHTML(el))
-    movie+=1
+parametersMoviesUpdate = {
+    container:document.querySelector('div#update_trick_movies'),
+    property: document.querySelectorAll("[data-prototype]"),
 }
 
-function createElementFromHTML(htmlString) {
-    let div = document.createElement('div');
-    div.innerHTML = htmlString.trim();
-    return div.firstChild;
+
+
+if (document.querySelector('div#add_trick_pictures')) {
+    let indexMovies = 0;
+
+    function addMoviesAdd() {
+        const template = parametersMovies.container.dataset.prototype
+        const el = template.replace(/__name__label__/g, 'image n°' + (indexMovies + 1))
+            .replace(/__name__/g, indexMovies)
+        parametersMovies.container.appendChild(createElementFromHTML(el))
+        indexMovies += 1
+    }
+
+
+    function createElementFromHTML(htmlString) {
+        let div = document.createElement('div');
+        div.innerHTML = htmlString.trim();
+        return div.firstChild;
+    }
+
+    if (indexMovies === 0) {
+        addMoviesAdd()
+    }
+
+    let buttonMovies = document.querySelector('#add_movies');
+
+    buttonMovies.addEventListener('click', function () {
+        addMoviesAdd()
+    });
 }
 
-if (movie === 0){
-    addMovie()
+if (document.querySelector('div#update_trick_pictures')) {
+    let indexMovies = 0;
+
+    function addMoviesUpdate() {
+        const template = parametersMoviesUpdate.container.dataset.prototype
+        const el = template.replace(/__name__label__/g, 'image n°' + (indexMovies + 1))
+            .replace(/__name__/g, indexMovies)
+        parametersMoviesUpdate.container.appendChild(createElementFromHTMLUpdate(el))
+        indexMovies += 1
+    }
+
+
+    function createElementFromHTMLUpdate(htmlString) {
+        let div = document.createElement('div');
+        div.innerHTML = htmlString.trim();
+        return div.firstChild;
+    }
+
+    if (indexMovies === 0) {
+        addMoviesUpdate()
+    }
+
+    let buttonMovies = document.querySelector('#update_movies');
+
+    buttonMovies.addEventListener('click', function () {
+        addMoviesUpdate()
+    });
 }
