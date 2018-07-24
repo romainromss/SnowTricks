cbbd11a8dd88a1ae404cef435b853826cb53d781
romain <romain.romss@gmail.com>
2018-07-24 09:50:23 +0200

    fix(mapping): change bi-directional tricks and pictures by uni-directional

--- config/bundles.php
+++ config/bundles.php
@@ -6,7 +6,7 @@ return [
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineCacheBundle\DoctrineCacheBundle::class => ['all' => true],
-	Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
     Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
